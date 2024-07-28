bcd3f488bea11aff31def4b3c5a57112d95ecfa7
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-07-28 09:06:26 +0700

    composition pt. 1

---
+++ ComputerFactory/src/PersonalComputer.java
@@ -0,0 +1,24 @@
+public class PersonalComputer extends Product {
+    private Monitor monitor;
+    private MotherBoard motherBoard;
+    private ComputerCase computerCase;
+
+    public PersonalComputer(String model, String manufacturer, Monitor monitor, MotherBoard motherBoard, ComputerCase computerCase) {
+        super(model, manufacturer);
+        this.monitor = monitor;
+        this.motherBoard = motherBoard;
+        this.computerCase = computerCase;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public MotherBoard getMotherBoard() {
+        return motherBoard;
+    }
+
+    public ComputerCase getComputerCase() {
+        return computerCase;
+    }
+}
