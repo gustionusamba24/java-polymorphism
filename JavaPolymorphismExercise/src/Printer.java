f3b75db2fb2ac9740355aa7ce55bfcdc145daeeb
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-07-29 13:41:31 +0700

    finish printer challenge

---
+++ JavaPolymorphismExercise/src/Printer.java
@@ -0,0 +1,35 @@
+public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        this.tonerLevel = (tonerLevel >= 0 && tonerLevel <= 100) ? tonerLevel : -1;
+        this.pagesPrinted = 0;
+        this.duplex = duplex;
+    }
+
+    public int addToner(int tonerAmount) {
+        int tempTonerLevel = tonerLevel + tonerAmount;
+        if (tempTonerLevel < 1 || tempTonerLevel > 100) {
+            return -1;
+        }
+
+        tonerLevel += tonerAmount;
+        return tonerLevel;
+    }
+
+    public int printPages(int pages) {
+        int pagesToPrint = pages;
+        if (duplex) {
+            pagesToPrint = (pages / 2) + (pages % 2);
+            System.out.println("Printing in duplex mode");
+        }
+        pagesPrinted += pagesToPrint;
+        return pagesToPrint;
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+}
