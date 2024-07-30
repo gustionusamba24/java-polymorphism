eb96b7841a58e7f71df4f5767a1c61ba8d134b43
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-07-30 20:56:36 +0700

    polymorphism challenge

--- PolymorphismChallenge/src/Car.java
+++ PolymorphismChallenge/src/Car.java
@@ -43,3 +43,56 @@ class GasPoweredCar extends Car {
         System.out.printf("Gas -> usage exceeds the average: %.2f %n", avgKmPerLiter);
     }
 }
+
+class ElectricCar extends Car {
+    private double avgKmPerCharge;
+    private int batterySize = 6;
+
+    public ElectricCar(String description) {
+        super(description);
+    }
+
+    public ElectricCar(String description, double avgKmPerCharge, int batterySize) {
+        super(description);
+        this.avgKmPerCharge = avgKmPerCharge;
+        this.batterySize = batterySize;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.printf("BEV -> switch %d kWh battery on, Ready!%n", batterySize);
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.printf("BEV -> usage under average: %.2f %n", avgKmPerCharge);
+    }
+}
+
+class HybridCar extends Car {
+    private double avgKmPerLitre;
+    private int batterySize;
+    private int cylinders = 6;
+
+    public HybridCar(String description) {
+        super(description);
+    }
+
+    public HybridCar(String description, double avgKmPerLitre, int batterySize, int cylinders) {
+        super(description);
+        this.avgKmPerLitre = avgKmPerLitre;
+        this.batterySize = batterySize;
+        this.cylinders = cylinders;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.printf("Hybrid -> All %d cylinders are fired up, Ready!%n", cylinders);
+        System.out.printf("Hybrid -> switch %d kWh battery on, Ready!%n", batterySize);
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.printf("Hybrid -> usage below the average: %.2f %n", avgKmPerLitre);
+    }
+}
--- PolymorphismChallenge/src/Main.java
+++ PolymorphismChallenge/src/Main.java
@@ -5,6 +5,12 @@ public class Main {
 
         Car ferrari = new GasPoweredCar("2022 Blue Ferrari 296 GTS", 10.5, 6);
         runRace(ferrari);
+
+        Car tesla = new ElectricCar("2022 Red Tesla Model S", 568, 75);
+        runRace(tesla);
+
+        Car innovaZenix = new HybridCar("2022 Silver Toyota Innova Zenix", 12.5, 1, 6);
+        runRace(innovaZenix);
     }
 
     public static void runRace(Car car) {
