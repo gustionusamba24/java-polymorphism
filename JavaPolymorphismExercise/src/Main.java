9d61bcbcdee9a2b09bdee3eb71b118e0c55320d5
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-07-30 20:04:27 +0700

    finish polymorphism exercise

--- JavaPolymorphismExercise/src/Main.java
+++ JavaPolymorphismExercise/src/Main.java
@@ -16,12 +16,32 @@ public class Main {
 //
 //        bedRoom.getLamp().turnOn();
 
-        Printer printer = new Printer(50, true);
-        System.out.println(printer.addToner(50));
-        System.out.println("initial page count = " + printer.getPagesPrinted());
-        int pagesPrinted = printer.printPages(4);
-        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
-        pagesPrinted = printer.printPages(2);
-        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
+//        Printer printer = new Printer(50, true);
+//        System.out.println(printer.addToner(50));
+//        System.out.println("initial page count = " + printer.getPagesPrinted());
+//        int pagesPrinted = printer.printPages(4);
+//        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
+//        pagesPrinted = printer.printPages(2);
+//        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
+
+        Car car = new Car(8, "Base car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRX 4WD");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+
+        Ford ford = new Ford(6, "Ford Falcon");
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+
+        Holden holden = new Holden(6, "Holden Commodore");
+        System.out.println(holden.startEngine());
+        System.out.println(holden.accelerate());
+        System.out.println(holden.brake());
     }
 }
